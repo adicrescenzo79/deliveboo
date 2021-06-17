bffff71d73bb4723d350027bb12e18adb67de7b3
Vsevolod <cerneavschivsevolod@gmail.com>
2021-06-17 23:27:38 +0200

    Aggiunto il file restaurants.php lella cartella config

---
+++ config/restaurants.php
@@ -0,0 +1,15 @@
+<?php
+
+$restaurants = [
+  [
+    "user_id" => ,
+    "name" => "",
+    "slug" => "",
+    "logo" => "",
+    "cover_image" => "",
+    "address" => "",
+    "p_iva" => "",
+    "phone" => "",
+
+  ]
+]
