d5d5a68d9e641e2885841ae420a33ba345019a26
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-06-18 16:50:21 +0200

    creti migration tabella pivot categoria ristorante. iniziato inseriimento dati

---
+++ config/categories.php
@@ -0,0 +1,35 @@
+<?php
+
+return $categories = [
+  [
+    "name" => "Italiano",
+    "icon" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,h_180,q_auto,w_290/v1/it/cuisine-icons/italiano",
+  ],
+  [
+    "name" => "Pizza",
+    "icon" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,h_180,q_auto,w_290/v1/it/cuisine-icons/pizza",
+  ],
+  [
+    "name" => "Cinese",
+    "icon" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,h_180,q_auto,w_290/v1/it/cuisine-icons/cinese",
+  ],
+  [
+    "name" => "Giapponese",
+    "icon" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,h_180,q_auto,w_290/v1/it/cuisine-icons/sushi",
+  ],
+  [
+    "name" => "Fast Food",
+    "icon" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,h_180,q_auto,w_290/v1/it/cuisine-icons/hamburger",
+  ],
+  [
+    "name" => "Messicano",
+    "icon" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,h_180,q_auto,w_290/v1/it/cuisine-icons/messicano",
+  ],
+  [
+    "name" => "Indiano",
+    "icon" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,h_180,q_auto,w_290/v1/it/cuisine-icons/indiano",
+  ],
+
+];
+
+?>
