58c1a70f6a04ac8bc03046590494ea3eea27230b
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-06-18 12:05:27 +0200

    Settaggio migrate + fillable model

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+  protected $fillable = [
+    'name',
+    'slug',
+    'telephone',
+    'address',
+    'p_iva',
+    'logo',
+    'cover_image'
+  ];
 }
--- database/migrations/2021_06_18_094722_create_restaurants_table.php
+++ database/migrations/2021_06_18_094722_create_restaurants_table.php
@@ -15,6 +15,13 @@ class CreateRestaurantsTable extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('telephone');
+            $table->string('address');
+            $table->string('p_iva');
+            $table->text('logo');
+            $table->text('cover_image')->nullable();
             $table->timestamps();
         });
     }
