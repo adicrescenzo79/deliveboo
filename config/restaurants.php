4d94fec1e1f535e90da5b9dab2bb544152f69018
Vsevolod <cerneavschivsevolod@gmail.com>
2021-06-18 11:17:35 +0200

    commit errato

--- config/restaurants.php
+++ config/restaurants.php
@@ -2,14 +2,14 @@
 
 $restaurants = [
   [
-    "user_id" => ,
-    "name" => "",
-    "slug" => "",
-    "logo" => "",
-    "cover_image" => "",
-    "address" => "",
-    "p_iva" => "",
-    "phone" => "",
+    "user_id" => 1,
+    "name" => "La molisana",
+    "slug" => "la-molisana",
+    "logo" => "#",
+    "cover_image" => "#",
+    "address" => "via roma",
+    "p_iva" => "555544554555",
+    "phone" => "95583355665",
 
   ]
-]
+];
