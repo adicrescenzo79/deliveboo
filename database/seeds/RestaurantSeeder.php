37fd15957f3ffd025d5d2a54c759a667c9b292a1
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-06-20 17:14:39 +0200

    creato seeder ristoranti

--- database/seeds/RestaurantSeeder.php
+++ database/seeds/RestaurantSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Restaurant;
 
 class RestaurantSeeder extends Seeder
 {
@@ -11,6 +12,18 @@ class RestaurantSeeder extends Seeder
      */
     public function run()
     {
-        //
+      $restaurants = config(restaurants);
+
+      foreach ($restaurants as $restaurant) {
+        $newRestaurant = new Restaurant();
+        $newRestaurant->name = $restaurant['name'];
+        $newRestaurant->slug = $restaurant['slug'];
+        $newRestaurant->telephone = $restaurant['telephone'];
+        $newRestaurant->address = $restaurant['address'];
+        $newRestaurant->p_iva = $restaurant['p_iva'];
+        $newRestaurant->logo = $restaurant['logo'];
+        $newRestaurant->cover_image = $restaurant['cover_image'];
+        $newStudent->save();
+      }
     }
 }
