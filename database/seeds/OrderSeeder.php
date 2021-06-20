51c275660fa967cd6c38a27950001a5db319ddbc
Andrea Marangon <marangonandrea84@gmail.com>
2021-06-20 17:07:06 +0200

    correzione seeds di order

--- database/seeds/OrderSeeder.php
+++ database/seeds/OrderSeeder.php
@@ -13,17 +13,17 @@ class OrderSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-      for ($1 = 0; $i < 100; $1++){
-        $newStudent = new Student();
-        $newStudent->name = $faker->name();
-        $newStudent->customer_email = $faker->email();
-        $newStudent->customer_telephone = '+39 02 ' . $faker->randomNumber(7,true);
-        $newStudent->delivery_address = $faker->streatAddress();
-        $newStudent->delivery_time = $faker->time();
-        $newStudent->order_number = '#' . $faker->randomNumber(4,true);
-        $newStudent->total_paid = $faker->randomFloat(2,25,150);
-        $newStudent->created_at = $faker->date();
-        $newStudent->updated_at = $newStudent->created_at;
+      for ($i = 0; $i < 100; $i++){
+        $newOrder = new Order();
+        $newOrder->name = $faker->name();
+        $newOrder->customer_email = $faker->email();
+        $newOrder->customer_telephone = '+39 02 ' . $faker->randomNumber(7,true);
+        $newOrder->delivery_address = $faker->streatAddress();
+        $newOrder->delivery_time = $faker->time();
+        $newOrder->order_number = '#' . $faker->randomNumber(4,true);
+        $newOrder->total_paid = $faker->randomFloat(2,25,150);
+        $newOrder->created_at = $faker->date();
+        $newOrder->updated_at = $newOrder->created_at;
       }
     }
 }
