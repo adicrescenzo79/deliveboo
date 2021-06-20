67ea26c987d5e157e78d6c9b11ddd901dd376dff
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-06-20 11:17:53 +0200

    iseriti piatti del ristorante indiano

--- config/dishes.php
+++ config/dishes.php
@@ -2,11 +2,135 @@
 
 return $dishes = [
   [
-    "name" => "", //char
-    "description" => "", //string
-    "price" => ,//number
+    "name" => "vegetable samosa", //char
+    "description" => "involtino con ripieno di patate e verdure speziate", //string
+    "price" => 1.50,//number
     "image" => "",//link
-    "visibility" => ,// 0-1
-    "restaurant_id" => , //smallinteger
-  ]
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 25, //smallinteger
+  ],
+  [
+    "name" => "vegetarian curry", //char
+    "description" => "verdure miste cotte in una miscela speciale con curry", //string
+    "price" => 5.90,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 25, //smallinteger
+  ],
+  [
+    "name" => "vegetable biryani", //char
+    "description" => "fragrante di riso basmati cotto con verdure miste", //string
+    "price" => 7.00,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 25, //smallinteger
+  ],
+  [
+    "name" => "riso bianco", //char
+    "description" => "riso basmati cotto al vapore", //string
+    "price" => 6.50,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 25, //smallinteger
+  ],
+  [
+    "name" => "menu fisso", //char
+    "description" => "samosa, pakora, seekh kebab, chicken tikka, chicken curry, riso bianco, pane naan e dlce gulab jamun", //string
+    "price" => 15.00,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 26, //smallinteger
+  ],
+  [
+    "name" => "menu fisso vegetariano", //char
+    "description" => "samosa, pakora, palak pakora, baingan bharta, pane naan e dolce indiano", //string
+    "price" => 11.50,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 26, //smallinteger
+  ],
+  [
+    "name" => "samosa", //char
+    "description" => "fagottino ripieno di verdure cotte", //string
+    "price" => 1.70,//number
+    "image" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,q_auto,h_100,w_134,dpr_1.0/v1/it/dishes/201104/samosa.jpg",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 26, //smallinteger
+  ],
+  [
+    "name" => "seekh kebabs pollo", //char
+    "description" => "involtini carne macinata di pollo spezie e cotti nel forno tandoori", //string
+    "price" => 7.70,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 26, //smallinteger
+  ],
+  [
+    "name" => "menu seciale", //char
+    "description" => "antipasto speciale, pollo o agnello o gamberoni tikka masala, riso verdure e cheese nan", //string
+    "price" => 15.00,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 27, //smallinteger
+  ],
+  [
+    "name" => "antipasto speciale", //char
+    "description" => "per una persona", //string
+    "price" => 6.95,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 27, //smallinteger
+  ],
+  [
+    "name" => "insalata mista", //char
+    "description" => "", //string
+    "price" => 3.00,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 27, //smallinteger
+  ],
+  [
+    "name" => "raj mix tandoori", //char
+    "description" => "squisito assortimento di specialità tandoori", //string
+    "price" => 14.95,//number
+    "image" => "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,q_auto,h_100,w_134,dpr_1.0/v1/it/dishes/201562/raj-mix-tandoori.jpg",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 27, //smallinteger
+  ],
+  [
+    "name" => "componi il tuo menu pollo", //char
+    "description" => "un antipasto, un piatto principale, un riso, un pane e una bibita", //string
+    "price" => 14.95,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 28, //smallinteger
+  ],
+  [
+    "name" => "antipasto speciale", //char
+    "description" => "misto con chicken tikka, gamberoni butterfly, veg pakora e samosa vegetables", //string
+    "price" => 5.95,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 28, //smallinteger
+  ],
+  [
+    "name" => "raj special biriany", //char
+    "description" => "agnello, pollo e gamberoni saltati con fragrante di riso basmati e con spezie indiane delicate, servite ocn salsa di verdure o salsa raitha a parte", //string
+    "price" => 12.00,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 28, //smallinteger
+  ],
+  [
+    "name" => "misto tandoore special", //char
+    "description" => "assortimento di bocconicni di petto di pollo, coscia di pollo, agnello kebab e gambaroni argentini cotti al forno tandoor", //string
+    "price" => 12.00,//number
+    "image" => "",//link
+    "visibility" => 1,// 0-1
+    "restaurant_id" => 28, //smallinteger
+  ],
+
+
+
+
 ]
