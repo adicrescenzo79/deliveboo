cef2888dbfa8af041d82ab1e4fc527ae0e5b1d2a
Vsevolod <cerneavschivsevolod@gmail.com>
2021-06-20 16:58:05 +0200

    Fatto il seeder del Dish

--- database/seeds/DishSeeder.php
+++ database/seeds/DishSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Dish;
 
 class DishSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class DishSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $dishes = config('dishes');
+
+        for ($i = 0; $i < count($dishes); $i++) {
+            $dish = $dishs[$i];
+            $dish_obj = new Dish();
+            $dish_obj->title = $dish['title'];
+            $dish_obj->description = $dish['description'];
+            $dish_obj->thumb = $dish['thumb'];
+            $dish_obj->price = $dish['price'];
+            $dish_obj->series = $dish['series'];
+            $dish_obj->sale_date = $dish['sale_date'];
+            $dish_obj->type = $dish['type'];
+            $dish_obj->save();
+        }
     }
 }
