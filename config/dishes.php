136b27837c07e704f43f76e3b2f56b7af0f74080
Vsevolod <cerneavschivsevolod@gmail.com>
2021-06-20 12:50:13 +0200

    Inseriti PIatti Mexican and FastFood

--- config/dishes.php
+++ config/dishes.php
@@ -8,5 +8,301 @@ return $dishes = [
     "image" => "",//link
     "visibility" => ,// 0-1
     "restaurant_id" => , //smallinteger
-  ]
+  ],
+
+   //Mexican and FastFood dishes
+   //Piatti per Messiacano(id.9 - id.12);
+   //messicano1 id-9:
+    [
+      "restaurant_id" => 9,
+      "name" => "Tamales",
+      "description" => "Impasto di mais cotto a vapore all’interno di foglie di banana, carne manzo e verdure",
+      "price" => 9.00 ,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fill,f_auto,q_auto/Stores/jnwlqyid66np06bhe58w",
+      "visibility" => "true", // bool
+    ],
+
+    [
+      "restaurant_id" => 9,
+      "name" => "Burrito cali",
+      "description" => "Tortilla di frumento arrotolata con ripieno di riso, spalla di maiale, bacon, fagioli neri e crema di edamer",
+      "price" => 8.40 ,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fill,f_auto,q_auto/Stores/t5wsocnlookkuqmntbr2",
+      "visibility" => "true",
+    ],
+    [
+      "restaurant_id" => 9 ,  //smallinteger
+      "name" => "Nachos", //char
+      "description" => "110gr di nachos di mais conditi con chili con carne, jalapenos, panna acida e crema di cheddar",
+      "price" => , 9.90, //number
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,f_auto,q_auto/Products/rf6uz6f9vgn88sdwglu4",
+      "visibility" => "1",
+    ],
+    [
+      "restaurant_id" => 9,  //smallinteger
+      "name" => "Enchiladas", //char
+      "description" => "Due tortillas di mais farcite con verdure e carne di pollo, ricoperte di salsa roja e formaggio, cotte al forno e servite con riso mex (piccante) e fagioli neri",   //string
+      "price" => 12.50,   //number
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,f_auto,q_auto/Products/zkz6fypxwjqgwez8umqf",   //link
+      "visibility" => 1,// bool
+    ]
+
+    //messicano2 id-10:
+     [
+       "restaurant_id" => 10,
+       "name" => "Tamales",
+       "description" => "Impasto di mais cotto a vapore all’interno di foglie di banana, carne manzo e verdure",
+       "price" => 9.60 ,
+       "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fill,f_auto,q_auto/Stores/jnwlqyid66np06bhe58w",
+       "visibility" => "true", // bool
+     ],
+
+     [
+       "restaurant_id" => 10,
+       "name" => "Burrito cali",
+       "description" => "Tortilla di frumento arrotolata con ripieno di riso, spalla di maiale, bacon, fagioli neri e crema di edamer",
+       "price" => 9.40 ,
+       "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fill,f_auto,q_auto/Stores/t5wsocnlookkuqmntbr2",
+       "visibility" => "true",
+     ],
+     [
+       "restaurant_id" => 10 ,  //smallinteger
+       "name" => "Nachos", //char
+       "description" => "110gr di nachos di mais conditi con chili con carne, jalapenos, panna acida e crema di cheddar",
+       "price" => , 8.90, //number
+       "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,f_auto,q_auto/Products/rf6uz6f9vgn88sdwglu4",
+       "visibility" => "true",
+     ],
+     [
+       "restaurant_id" => 10,  //smallinteger
+       "name" => "Enchiladas", //char
+       "description" => "Due tortillas di mais farcite con verdure e carne di pollo, ricoperte di salsa roja e formaggio, cotte al forno e servite con riso mex (piccante) e fagioli neri",   //string
+       "price" => 11.50,   //number
+       "image" => "https://res.cloudinary.com/glovoapp/w_100,f_auto,q_auto/Products/zkz6fypxwjqgwez8umqf",   //link
+       "visibility" => "true",// bool
+     ]
+
+     //messicano3 id-11:
+      [
+        "restaurant_id" => 11,
+        "name" => "Tamales",
+        "description" => "Impasto di mais cotto a vapore all’interno di foglie di banana, carne manzo e verdure",
+        "price" => 10.60 ,
+        "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fill,f_auto,q_auto/Stores/jnwlqyid66np06bhe58w",
+        "visibility" => "true", // bool
+      ],
+
+      [
+        "restaurant_id" => 11,
+        "name" => "Burrito cali",
+        "description" => "Tortilla di frumento arrotolata con ripieno di riso, spalla di maiale, bacon, fagioli neri e crema di edamer",
+        "price" => 10.40 ,
+        "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fill,f_auto,q_auto/Stores/t5wsocnlookkuqmntbr2",
+        "visibility" => "true",
+      ],
+      [
+        "restaurant_id" => "11" ,  //smallinteger
+        "name" => "Nachos", //char
+        "description" => "110gr di nachos di mais conditi con chili con carne, jalapenos, panna acida e crema di cheddar",
+        "price" => , 10.90, //number
+        "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,f_auto,q_auto/Products/rf6uz6f9vgn88sdwglu4",
+        "visibility" => "true",
+      ],
+      [
+        "restaurant_id" => 11,  //smallinteger
+        "name" => "Enchiladas", //char
+        "description" => "Due tortillas di mais farcite con verdure e carne di pollo, ricoperte di salsa roja e formaggio, cotte al forno e servite con riso mex (piccante) e fagioli neri",   //string
+        "price" =>  12.50,   //number
+        "image" => "https://res.cloudinary.com/glovoapp/w_100,f_auto,q_auto/Products/zkz6fypxwjqgwez8umqf",   //link
+        "visibility" => "true",// bool
+      ]
+
+      //messicano4 id-12:
+       [
+         "restaurant_id" => 12,
+         "name" => "Tamales",
+         "description" => "Impasto di mais cotto a vapore all’interno di foglie di banana, carne manzo e verdure",
+         "price" => 10.60 ,
+         "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fill,f_auto,q_auto/Stores/jnwlqyid66np06bhe58w",
+         "visibility" => "true", // bool
+       ],
+
+       [
+         "restaurant_id" => 12,
+         "name" => "Burrito cali",
+         "description" => "Tortilla di frumento arrotolata con ripieno di riso, spalla di maiale, bacon, fagioli neri e crema di edamer",
+         "price" => 10.40 ,
+         "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fill,f_auto,q_auto/Stores/t5wsocnlookkuqmntbr2",
+         "visibility" => "true",
+       ],
+       [
+         "restaurant_id" => 12 ,  //smallinteger
+         "name" => "Nachos", //char
+         "description" => "110gr di nachos di mais conditi con chili con carne, jalapenos, panna acida e crema di cheddar",
+         "price" => , 9.90, //number
+         "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,f_auto,q_auto/Products/rf6uz6f9vgn88sdwglu4",
+         "visibility" => "true",
+       ],
+       [
+         "restaurant_id" => 12,  //smallinteger
+         "name" => "Enchiladas", //char
+         "description" => "Due tortillas di mais farcite con verdure e carne di pollo, ricoperte di salsa roja e formaggio, cotte al forno e servite con riso mex (piccante) e fagioli neri",   //string
+         "price" => 12.50,   //number
+         "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100f_auto,q_auto/Products/zkz6fypxwjqgwez8umqf",   //link
+         "visibility" => "true",// bool
+       ],
+
+       //Piatti per FASTFOOD(id.21 - id.24);
+
+      //FASTFOOD id-21:
+      [
+      "restaurant_id" => 21,
+      "name" => "La Belpaese",
+      "description" => "Bresaola, Rucola, Grana",
+      "price" => 6.50,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,h_100,c_fit,f_auto,q_auto/Products/bm1vnxxzkeqjz3xwsldo",
+      "visibility" => "true",
+  ],
+  [
+      restaurant_id" => 21,
+      "name" => "Whopper",
+      "description" => "Classico irresistibile. La ricetta del leggendario Whopper®: carne di manzo alla griglia e ingredienti freschi per un sapore ineguagliabile.",
+      "price" => 5.60,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/ewuyc9piur8e79i7pntg",
+      "visibility" => "true",
+      ],
+      [
+
+      restaurant_id" => 21,
+      "name" => "Plant Based Whopper",
+      "description" => "L’iconico Whopper con una nuova patty a base vegetale, per un gusto 100% Whopper, 0% carne.",
+      "price" => 5.90,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/tunyfp2h3h9xcwvnal8e",
+      "visibility" => "true",
+  ],
+    [
+      restaurant_id" => 21,
+      "name" => "Crispy chicken",
+      "description" => "L’iconico Whopper con una nuova patty a base vegetale, per un gusto 100% Whopper, 0% carne.",
+      "price" => 4.50,
+      "image" => "https://res.cloudinary.com/glovoapp/w_600,h_500,c_fit,f_auto,q_auto/Products/bqt9nsz4nt3gduqzqx9i",
+      "visibility" => "true",
+  ],
+
+      //FASTFOOD id-22:
+      [
+      "restaurant_id" => 22,
+      "name" => "Wrap di Pollo Grigliato",
+      "description" => "Piadina con ripieno di pollo grigliato, maionese, lattuga fresca e pomodoro",
+      "price" => 5.40,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/rxu2olyi1thdbl4qommx",
+      "visibility" => "true",
+      ],
+      [
+
+      restaurant_id" => 22,
+      "name" => "Crispy chicken",
+      "description" => "Semplicità color oro. Filetto di pollo in croccante panatura speziata. Pomodoro, lattuga e maionese.",
+      "price" => 4.50,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/bqt9nsz4nt3gduqzqx9i",
+      "visibility" => "true",
+  ],
+  [
+      restaurant_id" => 22,
+      "name" => "Double cheeseburger",
+      "description" => "Golosità compatta. Compatto, semplice ma unico. Per la merenda o un pranzo più light, gusto assicurato!",
+      "price" => 3.90,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/msavqzewlukkowcpsl4z",
+      "visibility" => "true",
+      ],
+      [
+
+      restaurant_id" => 22,
+      "name" => "Big King",
+      "description" => "Non mi basta.Il nostro Big King con doppia carne di manzo alla griglia, doppio formaggio e deliziosa salsa King.",
+      "price" => 5.20,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/nuqxnasiho77s5g7oid3",
+      "visibility" => "true",
+
+  ],
+    [
+      //FASTFOOD id-23:
+      "restaurant_id" => 23,
+      "name" => "Friends Fries",
+      "description" => "Croccanti e deliziose, non potrai farne a meno!",
+      "price" => 3.40,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/x9itasgg8fhharuom2ts",
+      "visibility" => "true",
+  ],
+  [
+      restaurant_id" => 23,
+      "name" => "Chicken royale",
+      "description" => "Croccante leggerezza. Usiamo solo il nostro panino al sesamo extralungo, con tanto petto di pollo dorato.",
+      "price" => 5.20,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/sichqzffa7kzl8onmmaz",
+      "visibility" => "true",
+  ],
+  [
+
+      restaurant_id" => 23,
+      "name" => "Wrap di Pollo Grigliato",
+      "description" => "Piadina con ripieno di pollo grigliato, maionese, lattuga fresca e pomodoro",
+      "price" => 5.40,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/rxu2olyi1thdbl4qommx",
+      "visibility" => "true",
+      ],
+      [
+
+      restaurant_id" => 23,
+      "name" => "Plant Based Whopper",
+      "description" => "L’iconico Whopper con una nuova patty a base vegetale, per un gusto 100% Whopper, 0% carne.",
+      "price" => 5.90,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/tunyfp2h3h9xcwvnal8e",
+      "visibility" => "true",
+      ],
+
+
+      //FASTFOOD id-24:
+      [
+      "restaurant_id" => 24,
+      "name" => "Zinger",
+      "description" => "Only the brave! Delizioso filetto di pollo con panatura Hot & Spicy, lattuga, cheddar cheese e doppio strato di maionese, in un morbido panino al sesamo. Gusto estremo!",
+      "price" => 5.25,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/2047636477",
+      "visibility" => "true",
+      ],
+      [
+
+      restaurant_id" => 24,
+      "name" => "Double Krunch",
+      "description" => "La morbida croccantezza raddoppia con 2 Tenders Crispy! Pane con semi di sesamo, due Tenders Crispy, insalata, cheddar e salsa al pepe nero.",
+      "price" => 5.25,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/2047636475",
+      "visibility" => "true",
+      ],
+      [
+
+      restaurant_id" => 24,
+      "name" => "Colonel's Burger",
+      "description" => "Il burger del Colonnello. Bacon, formaggio, insalata, salsa barbecue, maionese e un irresistibile filetto Original Recipe, preparato secondo la sua leggendaria ricetta, con il mix segreto di 11 erbe e spezie.",
+      "price" => 5.70,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/2047636479",
+      "visibility" => "true",
+      ],
+      [
+
+      restaurant_id" => 24,
+      "name" => "Double cheeseburger",
+      "description" => "Golosità compatta. Compatto, semplice ma unico. Per la merenda o un pranzo più light, gusto assicurato!",
+      "price" => 3.90,
+      "image" => "https://res.cloudinary.com/glovoapp/w_100,c_fit,f_auto,q_auto/Products/msavqzewlukkowcpsl4z",
+      "visibility" => "true",
+      ],
+
+
+
+
+
+
+
 ]
