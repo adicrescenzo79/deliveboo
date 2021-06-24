195c9bfba39b652e7a032728155c5d3d375e26a0
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-06-24 15:15:41 +0200

    Inizio show dishes

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -10,10 +10,25 @@
             <div class="card-header"><h5>{{$dish->name}}</h5></div>
             <div class="card-body">
               @if($dish->image)
-              <img src="{{asset($dish->image)}}" alt="{{$dish->image}}">
+                <img src="{{asset($dish->image)}}" alt="{{$dish->image}}">
               @endif
+
+              <h2>{{$dish->name}}</h2>
+
+              @if ($dish->description)
+              <p>{{$dish->description}}</p>
+              @endif
+
+              <p>{{$dish->price.'€'}}</p>
+
+              {{-- DA GESTIRE --}}
+              <input type="checkbox" name="" value="" >
+              Disponibile
             </div>
           </div>
+          
+          <a href="{{route('admin.dishes.edit', ['dish'=>$dish->id])}}"><button class="btn btn-primary" type="button" name="button">Modifica</button></a>
+
         </div>
     </div>
   </div>
