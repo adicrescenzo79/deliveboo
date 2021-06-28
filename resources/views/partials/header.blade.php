292dcf5ba040fa372df0f6ed4b1a0c57bf489692
Andrea Marangon <marangonandrea84@gmail.com>
2021-06-28 12:58:14 +0200

    lavoro su navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,55 +1,55 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="navbar navbar-expand-md navbar-light shadow-sm bg-light fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="{{ url('/') }}">
+      <img src="https://loghi-famosi.com/wp-content/uploads/2021/01/Uber-Eats-Logo.png" alt="">
+    </a>
+    {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+      <span class="navbar-toggler-icon"></span>
+    </button> --}}
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
+      {{-- <ul class="navbar-nav mr-auto">
 
-            </ul>
+      </ul> --}}
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Authentication Links -->
+        @guest
+          <li class="nav-item">
+            <a class="my-btn" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+          </li>
+          @if (Route::has('register'))
+            <li class="nav-item">
+              <a class="my-btn" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+            </li>
+          @endif
+        @else
+          <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              {{ Auth::user()->name }}
+            </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+            </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+            </form>
 
-                            {{-- <a href="{{route('')}}">Area personale</a> --}}
+            {{-- <a href="{{route('')}}">Area personale</a> --}}
 
-                            <a class="dropdown-item" href="{{route('admin.restaurants.index')}}">I tuoi ristoranti</a>
+            <a class="dropdown-item" href="{{route('admin.restaurants.index')}}">I tuoi ristoranti</a>
 
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
-    </div>
+          </div>
+        </li>
+      @endguest
+    </ul>
+  </div>
+</div>
 </nav>
