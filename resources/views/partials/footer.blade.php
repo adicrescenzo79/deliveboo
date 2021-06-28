9fb810afded58d2634d67988ef9d13f275788864
Andrea Marangon <marangonandrea84@gmail.com>
2021-06-28 16:47:30 +0200

    footer terminato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,12 @@
-<h1>footer</h1>
+<footer>
+  <div class="container-footer">
+    <div class="logo-footer">
+      <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/c266ad32e5e88af804b3a1b6b60098f9.svg" alt="">
+    </div>
+    <ul class="social-icon">
+      <li><i class="fab fa-facebook-square"></i></li>
+      <li><i class="fab fa-twitter"></i></li>
+      <li><i class="fab fa-instagram"></i></li>
+    </ul>
+  </div>
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,9 +3,9 @@
     <a class="navbar-brand" href="{{ url('/') }}">
       <img src="https://loghi-famosi.com/wp-content/uploads/2021/01/Uber-Eats-Logo.png" alt="">
     </a>
-    {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
       <span class="navbar-toggler-icon"></span>
-    </button> --}}
+    </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
