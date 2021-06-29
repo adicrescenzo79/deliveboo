13447a6a9caab635caa5b76c8bb58bfc48bb0889
Andrea Marangon <marangonandrea84@gmail.com>
2021-06-29 19:22:29 +0200

    finita vista register e lavoro su vista admin/restaurants

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,35 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="home">
+<div id="main-admin-restaurants">
 
   <div class="container">
     <div class="row justify-content-center">
+      <div class="col-md-12">
+        <a href="{{route('admin.restaurants.create')}}"><button class="btn btn-primary" type="button" name="button">Crea</button></a>
+      </div>
       @foreach ($restaurants as $restaurant)
-        <div class="col-md-6">
+        <div class="col-md-8">
           <div class="card">
-            <div class="card-header">{{$restaurant->name}}</div>
-            <div class="card-body">
-              <a href="{{route('admin.restaurants.edit', ['restaurant'=>$restaurant->id])}}"><button class="btn btn-primary" type="button" name="button">Modifica</button></a>
-              <form class="form-delete" action="{{route('admin.restaurants.destroy', ['restaurant'=>$restaurant->id])}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <input type="submit" name="" value="Cancella">
-              </form>
-              <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}">Vedi Dettaglio</a>
-              {{-- <a href="{{ route('admin.restaurants.stats', ['restaurant' => $restaurant->id])}}">Vedi Statistiche</a> --}}
+            {{-- <div class="card-header"><h5>{{$restaurant->name}}</h5></div> --}}
+            <div class="card-body row">
+
+              <div class="card-img col-md-2">
+                <img src="{{$restaurant->logo}}" alt="">
+              </div>
+
+              <div class="col-md-5">
+                <ul>
+                  <li><strong>{{$restaurant->name}}</strong></li>
+                  <li>{{$restaurant->address}}</li>
+                  <li>P.IVA: {{$restaurant->p_iva}}</li>
+                  <li>Telefono: {{$restaurant->telephone}}</li>
+                </ul>
+              </div>
+
+              <div class="col-md-3">
+                <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}"><button class="my-btn-card" type="button" name="button">Ristorante</button></a>
+                <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}"><button class="my-btn-card" type="button" name="button">Statistiche</button></a>
+              </div>
+
+              <div class="col-md-2">
+                <a href="{{route('admin.restaurants.edit', ['restaurant'=>$restaurant->id])}}"><i class="fas fa-edit"></i></a>
+                <form class="form-delete" action="{{route('admin.restaurants.destroy', ['restaurant'=>$restaurant->id])}}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <input class="fa trash-icon" type="submit" name="" value="&#xf1f8;">
+                </form>
+              </div>
 
             </div>
           </div>
         </div>
       @endforeach
-      <a href="{{route('admin.restaurants.create')}}"><button class="btn btn-primary" type="button" name="button">Crea</button></a>
 
-      <div class="col-md-6">
-        <div class="card">
-
-        </div>
-      </div>
     </div>
   </div>
 </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light shadow-sm bg-light fixed-top">
   <div class="container">
     <a class="navbar-brand" href="{{ url('/') }}">
-      <img src="./img/logo-seven-eats.png" alt="">
+      <img src="{{ asset('img/logo-seven-eats.png')}}" alt="">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
       <span class="navbar-toggler-icon"></span>
