a218ef046b20621d27de7b55e6181c72c39cf296
Andrea Marangon <marangonandrea84@gmail.com>
2021-06-29 11:11:37 +0200

    avanzamento vista login + varie

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container pt-5">
+  <div id="main-login" class="container pt-5">
     <div class="row justify-content-center">
-      <div class="col-md-8">
+      <div class="col-md-6">
         <div class="card">
           <div class="card-header">{{ __('Ti diamo il bentornato') }}</div>
 
@@ -12,9 +12,9 @@
               @csrf
 
               <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                {{-- <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label> --}}
 
-                <div class="col-md-6">
+                <div class="col-md-12">
                   <input id="email" placeholder="Indirizzo email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                     @error('email')
@@ -26,9 +26,9 @@
                 </div>
 
                 <div class="form-group row">
-                  <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                  {{-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label> --}}
 
-                  <div class="col-md-6">
+                  <div class="col-md-12">
                     <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                       @error('password')
@@ -39,8 +39,8 @@
                     </div>
                   </div>
 
-                  <div class="form-group row">
-                    <div class="col-md-6 offset-md-4">
+                  {{-- <div class="form-group row">
+                    <div class="col-md-6">
                       <div class="form-check">
                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -49,19 +49,20 @@
                         </label>
                       </div>
                     </div>
-                  </div>
+                  </div> --}}
 
                   <div class="form-group row mb-0">
-                    <div class="col-md-8 offset-md-4">
-                      <button type="submit" class="btn btn-primary">
+                    <div class="col-md-12">
+                      <button type="submit" class="my-btn-login">
                         {{ __('Login') }}
                       </button>
 
-                      @if (Route::has('password.request'))
+                      {{-- @if (Route::has('password.request'))
                         <a class="btn btn-link" href="{{ route('password.request') }}">
                           {{ __('Forgot Your Password?') }}
                         </a>
-                      @endif
+                      @endif --}}
+                      <p>ciao</p>
                     </div>
                   </div>
                 </form>
