6e8c18f5971d8dbcb312ce2f1a11e511723d154b
Andrea Marangon <marangonandrea84@gmail.com>
2021-06-30 15:32:54 +0200

    vista admin/restaurants terminata

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -14,30 +14,29 @@
             {{-- <div class="card-header"><h5>{{$restaurant->name}}</h5></div> --}}
             <div class="card-body row">
 
-              <div class="card-img col-md-2">
+              <div class="card-img col-md-3 col-sm-12">
                 <img src="{{$restaurant->logo}}" alt="">
               </div>
 
               <div class="col-md-5 col-sm-12">
                 <ul>
-                  <li><strong>{{$restaurant->name}}</strong></li>
+                  <li class="pb-3"><strong>{{$restaurant->name}}</strong></li>
                   <li>{{$restaurant->address}}</li>
                   <li>P.IVA: {{$restaurant->p_iva}}</li>
                   <li>Telefono: {{$restaurant->telephone}}</li>
                 </ul>
               </div>
 
-              <div class="col-md-3 col-sm-6 col-6 container-buttons">
-                <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}"><button class="my-btn-card" type="button" name="button">Ristorante</button></a>
-                <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}"><button class="my-btn-card" type="button" name="button">Statistiche</button></a>
-              </div>
-
-              <div class="col-md-2 col-sm-6 col-6 container-icon-edit-trash">
-                <a href="{{route('admin.restaurants.edit', ['restaurant'=>$restaurant->id])}}"><i class="fas fa-edit"></i></a>
+              <div class="col-md-4 col-sm-12 container-buttons py-2">
+                {{-- <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}"><button class="my-btn-card" type="button" name="button">Ristorante</button></a>
+                <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}"><button class="my-btn-card" type="button" name="button">Statistiche</button></a> --}}
+                <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}"><i class="fas fa-utensils" title="Ristorante"></i></a>
+                <a href="{{ route('admin.restaurants.show', ['restaurant' => $restaurant->id])}}"><i class="far fa-chart-bar" title="Statistiche"></i></a>
+                <a href="{{route('admin.restaurants.edit', ['restaurant'=>$restaurant->id])}}"><i class="fas fa-edit" title="Modifica"></i></a>
                 <form class="form-delete" action="{{route('admin.restaurants.destroy', ['restaurant'=>$restaurant->id])}}" method="POST">
                   @csrf
                   @method('DELETE')
-                  <input class="fa trash-icon" type="submit" name="" value="&#xf1f8;">
+                  <input class="fa trash-icon" type="submit" title="Cancella" name="" value="&#xf1f8;">
                 </form>
               </div>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse position-relative" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
       {{-- <ul class="navbar-nav mr-auto">
 
