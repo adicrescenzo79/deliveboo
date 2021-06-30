277adb5fe4e213653e7ae1357553c5bdad939b46
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-06-30 18:04:38 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -190,7 +190,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Utente\Desktop\progetto_finale\deliveboo\resources\js\welcome.js */"./resources/js/welcome.js");
+module.exports = __webpack_require__(/*! /Users/nico/Desktop/Boolean/Esercitazioni/deliveboo/resources/js/welcome.js */"./resources/js/welcome.js");
 
 
 /***/ })
