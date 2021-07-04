2c07a19857fd337b9322138950e9cddd0e9a915f
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-07-04 17:02:39 +0200

    Realizzazione grafica statistiche

--- resources/views/admin/statistics/index.blade.php
+++ resources/views/admin/statistics/index.blade.php
@@ -5,7 +5,8 @@
     <div class="container px-3">
       <div class="row justify-content-center">
         <div class="col-md-12 flex justify-content-center flex-column align-items-center">
-          <div class="flex flex-row flex-wrap justify-content-center">
+          <h1>Seleziona l'anno</h1>
+          <div class="flex flex-row flex-wrap justify-content-center mb-4">
             <button class="my-btn-black mx-1 mt-2" v-for="(year, i) in years" @click="scelta(years[i])" type="button" name="button">@{{years[i]}}</button>
           </div>
           <canvas id="myChart"></canvas>
